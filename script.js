7e86423f0e6559d8c6d5511a9de37f4ed2460100
Arm37e3 <82191653+Arm37e3@users.noreply.github.com>
2024-01-23 10:18:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+    
 const apikey = '<api key>';
 const content = document.getElementById('content');
 const yearSelect = document.getElementById('year');
@@ -24,6 +24,8 @@ const displayMovies = async (selectedYear) => {
         reting.innerHTML = `${M.vote_average}`;
         poster.src = `${urlPoster}${M.poster_path}`;
 
+
+        //click open ink 
         poster.addEventListener('click', async () => {
          window.open("https://youtu.be/mDa8FfZkntA?si=jZ3F8OZy_9_QxVQ4","_blank");
        });
